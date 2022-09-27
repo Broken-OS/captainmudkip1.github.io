7de6dc6fb56a45d0fa3f97958a723e504fcb0994
CaptainMudkip1 <73603426+CaptainMudkip1@users.noreply.github.com>
2022-09-27 09:58:29 -0400

    Update index.js

--- websitefixer/index.js
+++ websitefixer/index.js
@@ -1,7 +1,7 @@
 function reloadWeb() {
     src = document.getElementById("textbox").value;
-    //Old method:  src.replace(/&/g, "&amp;").replace(/</g, "&lt;").replace(/>/g, "&gt;").replace(/"/g, "&quot;").replace(/'/g, "&#039;");
-    src = DOMPurify.sanitize(src);
+    //src.replace(/&/g, "&amp;").replace(/</g, "&lt;").replace(/>/g, "&gt;").replace(/"/g, "&quot;").replace(/'/g, "&#039;");
+    //src = DOMPurify.sanitize(src);
     document.write('<title>Website fixer</title>');
     document.write('<link rel="shortcut icon" href="/img/gear.png">');
     document.write('<style>iframe {position:fixed; top:0; left:0; bottom:0; right:0; width:100%; height:100%; border:none; margin:0; padding:0; overflow:hidden; z-index:999999;}</style>');
