04554a84cb916f13ca1b3b7402f99bf7d93f5a61
BuildTools <73603426+CaptainMudkip1@users.noreply.github.com>
2022-12-20 22:49:29 -0500

    Update index.js

--- grade_calc/index.js
+++ grade_calc/index.js
@@ -1,17 +1,24 @@
-function gradeCalc(earned, total) {
-  pe = earned * 100;
-  x = pe / total;
-  return x
-}
 function CALC() {
-  e = document.getElementById("earned").value;
-  t = document.getElementById("total").value;
-  o = document.getElementById("outputText");
-  if(e == ""){o.innerHTML = "";}
-  else if(t == ""){o.innerHTML = "";}
-  else {
-    x = gradeCalc(e, t);
-    console.log(x);
-    o.innerHTML = "Your score was <b>" + x + "%</b>";
-  }
+    e = document.getElementById("earned").value;
+    t = document.getElementById("total").value;
+    o = document.getElementById("outputText");
+    msg = null;
+    if (e == "") { o.innerHTML = ""; } else if (t == "") { o.innerHTML = ""; } else {
+        x = ((e * 100) / t);
+        console.log(x);
+        if (x >= 0 && x <= 59.49) {
+            msg = "E";
+        } else if (x >= 59.50 && x <= 69.49) {
+            msg = "D";
+        } else if (x >= 69.50 && x <= 79.49) {
+            msg = "C";
+        } else if (x >= 79.50 && x <= 89.49) {
+            msg = "B";
+        } else if (x >= 89.50) {
+            msg = "A";
+        } else {
+            msg = "ERR"
+        }
+        o.innerHTML = "Your grade was: <b>" + msg + "</b> (" + x + "%)";
+    }
 }
